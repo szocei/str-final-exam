7113bdac652a9b492a6a9930e0035db3103603b2
szocei <szocei.zoltan@gmail.com>
2021-03-16 18:16:58 +0100

    törlés

--- src/app/page/user-list/user-list.component.ts
+++ src/app/page/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/service/user.service';
 
@@ -10,13 +11,22 @@ import { UserService } from 'src/app/service/user.service';
 })
 export class UserListComponent implements OnInit {
 
-  users$: Observable<User[]> = this.userService.getAll();
+  users$:BehaviorSubject<User[]>| Observable<User[]> = this.userService.userList$;
 
   constructor(
     private userService: UserService,
+    private router:Router,
   ) { }
 
   ngOnInit(): void {
+this.userService.getAll()
   }
+
+ onRemove(user:User):void {
+    this.userService.remove(user),
+    this.router.navigate(['user'])
+    
+  }
+
 
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../model/user';
 
 @Injectable({
@@ -14,12 +14,15 @@ export class UserService {
     private http: HttpClient
   ) { }
 
+  userList$:BehaviorSubject<User[]>=new BehaviorSubject<User[]>([]);
   /**
    * Get all users from the database.
    * @returns on observable with all users.
    */
-  getAll(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.endpoint}`);
+  getAll():void {
+     this.http.get<User[]>(`${this.endpoint}`).subscribe(
+      data => this.userList$.next(data)
+    )
   }
 
   /**
@@ -35,13 +38,28 @@ export class UserService {
    * Delete a user from the database.
    * The method is: this.http.delete
    */
+remove(user:User):void {
+  this.http.delete<User>(
+    `${this.endpoint}/${user.id}`
+    ).subscribe(
+      () =>this.getAll()
+    );
 
+  }
 
 
   /**
    * Create a user in the database.
    * The method is: this.http.post
    */
+     create(user:User):void {
+  this.http.post<User>(
+    `${this.endpoint}`,
+    user
+    ).subscribe(
+      () =>this.getAll()
+    ); 
+   }
 
 
 
@@ -49,5 +67,17 @@ export class UserService {
    * Update a user in the database.
    * The method is: this.http.patch
    */
+
+  update(user:User):void {
+  this.http.patch<User>(
+    `${this.endpoint}/${user.id}`,
+    user
+    ).subscribe(
+      () =>this.getAll()
+    );
+  }
+
+
+
 
 }
